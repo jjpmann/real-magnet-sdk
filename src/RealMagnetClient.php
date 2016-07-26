8fbfefbd958b5c19b52b4476fe7a1ad381986ee2
Jerry Price <jerryprice@lmo.com>
2016-07-26 12:16:37 -0400

    adding missing client

---
+++ src/RealMagnetClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RealMagnet;
+
+class RealMagnetClient extends \GuzzleHttp\Client
+{
+
+    protected $url = 'https://dna.magnetmail.net/ApiAdapter/Rest/';
+
+    public function __construct()
+    {
+        $config = [
+            'base_uri' => $this->url,
+            'timeout' => 2.0,
+            'defaults' => [
+                'headers' => [
+                    'content-type' => 'application/json', 
+                    'Accept' => 'application/json'
+                ],
+           ],
+        ];
+
+        parent::__construct($config);
+    }
+
+}
+
+
